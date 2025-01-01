663ef161476fd4f90b5a1d14cf0d27480167f84b
Gogul Baskaran <gogulbaskaran01@gmail.com>
2025-01-02 01:30:30 +0530

    Cors added

--- Gemfile
+++ Gemfile
@@ -40,3 +40,7 @@ end
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 gem 'bcrypt', '~> 3.1.7'
+
+gem 'jwt'
+
+gem 'rack-cors'
--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,16 +1,19 @@
-# Be sure to restart your server when you modify this file.
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  allow do
+    # Allow requests from the frontend app running on localhost:4200 (development)
+    origins 'http://localhost:4200'
 
-# Avoid CORS issues when API is called from the frontend app.
-# Handle Cross-Origin Resource Sharing (CORS) in order to accept cross-origin AJAX requests.
+    resource '*',
+      headers: :any,
+      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+  end
 
-# Read more: https://github.com/cyu/rack-cors
+  allow do
+    # Allow requests from api.unishala.in (production or staging)
+    origins 'https://api.unishala.in'
 
-# Rails.application.config.middleware.insert_before 0, Rack::Cors do
-#   allow do
-#     origins 'example.com'
-#
-#     resource '*',
-#       headers: :any,
-#       methods: [:get, :post, :put, :patch, :delete, :options, :head]
-#   end
-# end
+    resource '*',
+      headers: :any,
+      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+  end
+end
